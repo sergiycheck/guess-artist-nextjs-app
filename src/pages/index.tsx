24ccf1fdb416640084506431ac4b064181965f8c
serhii <sergiychech@gmail.com>
2023-03-15 00:14:20 +0200

    removed inter google font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import { Box } from "@chakra-ui/react";
 import { SharedHead } from "@/components/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <SharedHead />
-      <Box as="main" className={inter.className}>
+      <Box as="main">
         <Box textStyle="h2">Guess artist game home page</Box>
       </Box>
     </>
